19c64be2bd1e4d120005d9fddde7a255e0db9424
Ebraham Alskaf <73792019+drskaf@users.noreply.github.com>
2023-01-13 23:36:54 +0000

    Update stress_file_extract.py

--- stress_file_extract.py
+++ stress_file_extract.py
@@ -1,26 +1,24 @@
 import numpy as np
-import pandas as pd
 import os
 import pydicom
 import argparse
-from os import walk
 import glob
 import re
-import pickle
+import shutil
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--directory", required=True, help="path to input directory")
 ap.add_argument("-s", "--sequence", required=False, help="name of required sequence")
-ap.add_argument("-o", "--output", required=False, help="output directory")
 args = vars(ap.parse_args())
 
-paths = sorted(glob.glob(os.path.join(args["directory"], "*","*","*", "*.dcm")))
+dir_paths = sorted(glob.glob(os.path.join(args["directory"], "*")))
 
-for path in paths:
-    if re.match(args["sequence"], str(file.SeriesDescription), flags=re.I):
-        output_dir = args["output"]
-        fname = os.path.basename(path)
-        file_name = os.path.join(output_dir, fname)
-        file1 = open(fname, "w")
-        file1.write('file')
-        file1.close()
+for dir_path in dir_paths:
+    id = os.path.basename(dir_path)
+    dir = os.mkdir(id)
+    dcm_paths = sorted(glob.glob(os.path.join(dir_path, "*", "DICOM", "*.dcm")))
+
+    for dcm_path in dcm_paths:
+        file = pydicom.dcmread(dcm_path)
+        if re.match(args["sequence"], str(file.SeriesDescription), flags=re.I):
+            shutil.move(dcm_path, f"{id}")
