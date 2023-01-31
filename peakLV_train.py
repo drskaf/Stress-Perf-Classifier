6c988b60e85f53940798b32b8a525c9a7a233044
Ebraham Alskaf <73792019+drskaf@users.noreply.github.com>
2023-01-31 00:27:25 +0000

    Update and rename peak_lv.py to peakLV_train.py

--- peakLV_train.py
+++ peakLV_train.py
@@ -10,4 +10,15 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--directory", required=True, help="path to input directory")
 args = vars(ap.parse_args())
 
+# ### Main ###
+
+LABELS = '.../peakLV_labels.csv'
+label_file = pd.read_csv(LABELS)
+print(label_file.head())
+IMG_SIZE = 256
+
+perf_videos = []
 for root, dirs, files in os.walk(args["directory"], topdown=True):
+    for dir in dirs:
+        perf = utils.load_perfusion_data(os.path.join(root,dir))
+        perf_videos.append(perf)
