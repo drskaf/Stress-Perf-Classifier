01614e8505e5b1e71654cd1f6168a091fa8a5430
Ebraham Alskaf <73792019+drskaf@users.noreply.github.com>
2023-02-01 15:43:29 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -72,9 +72,4 @@ def load_perfusion_data(directory):
                 
                 return video
  
-def crop_center_square(frame):
-    y, x = frame.shape[0:2]
-    min_dim = min(y, x)
-    start_x = (x // 2) - (min_dim // 2)
-    start_y = (y // 2) - (min_dim // 2)
-    return frame[start_y : start_y + min_dim, start_x : start_x + min_dim]
+
