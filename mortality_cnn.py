8db3bccd5f105daa2cc0cc98b53cff24bb05e21e
Ebraham Alskaf <73792019+drskaf@users.noreply.github.com>
2023-02-08 14:50:47 +0000

    Update mortality_cnn.py

--- mortality_cnn.py
+++ mortality_cnn.py
@@ -1,3 +1,20 @@
-import tensorflow as tf
 import numpy as np
 import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import os
+import tf_cnns
+import utils
+import argparse
+
+INPUT_DIM = [256, 256]
+BATCH_SIZE = 32
+NUM_EPOCHS = 100
+STEP_PER_EPOCH = 50
+NO_VALID_STEPS = 20
+N_CLASSES = 2
+CHECKPOINT_PATH = os.path.join("model_weights", "cp-{epoch:02d}")
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--directory", required=True, help="path to input directory")
+args = vars(ap.parse_args())
