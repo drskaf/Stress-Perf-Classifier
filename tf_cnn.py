8d6cb7f3b7687053b8dcd2161f8135fab9f763df
Ebraham Alskaf <73792019+drskaf@users.noreply.github.com>
2023-02-09 21:57:16 +0000

    Update tf_cnn.py

--- tf_cnn.py
+++ tf_cnn.py
@@ -10,34 +10,73 @@ import time
 
 ''' Building AlexNet '''
 
-def AlexNet(INPUT_SHAPE, OUTPUT):   
-    model = Sequential([
-        Conv2D(filters=96, kernel_size=(11, 11), strides=(4,4)
-                            , activation='relu', input_shape=INPUT_SHAPE),
-        BatchNormalization(),
-        MaxPool2D(pool_size=(3,3), strides=(2,2)),
-        Conv2D(filters=256, kernel_size=(5,5), strides=(1,1),
-                            activation='relu',padding="same"),
-        BatchNormalization(),
-        MaxPool2D(pool_size=(3,3), strides=(2,2)),
-        Conv2D(filters=384, kernel_size=(3,3), strides=(1,1),
-                            activation='relu',padding="same"),
-        BatchNormalization(),
-        Conv2D(filters=384, kernel_size=(3,3), strides=(1,1),
-                            activation='relu',padding="same"),
-        BatchNormalization(),
-        Conv2D(filters=256, kernel_size=(3,3), strides=(1,1),
-                            activation='relu',padding="same"),
-        BatchNormalization(),
-        MaxPool2D(pool_size=(3,3), strides=(2,2)),
-        Flatten(),
-        Dense(4096, activation='relu'),
-        Dropout(0.5),
-        Dense(4096, activation='relu'),
-        Dense(0.5),
-        Dense(OUTPUT, activation='softmax')
-        ])
-    return model
+class AlexNet:
+	@staticmethod
+	def build(width, height, depth, classes, reg=0.0002):
+		# initialize the model along with the input shape to be
+		# "channels last" and the channels dimension itself
+		model = Sequential()
+		inputShape = (height, width, depth)
+		chanDim = -1
+
+		# if we are using "channels first", update the input shape
+		# and channels dimension
+		if K.image_data_format() == "channels_first":
+			inputShape = (depth, height, width)
+			chanDim = 1
+
+		# Block #1: first CONV => RELU => POOL layer set
+		model.add(Conv2D(96, (11, 11), strides=(4, 4),
+			input_shape=inputShape, padding="same",
+			kernel_regularizer=l2(reg)))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+		model.add(Dropout(0.25))
+
+		# Block #2: second CONV => RELU => POOL layer set
+		model.add(Conv2D(256, (5, 5), padding="same",
+			kernel_regularizer=l2(reg)))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+		model.add(Dropout(0.25))
+
+		# Block #3: CONV => RELU => CONV => RELU => CONV => RELU
+		model.add(Conv2D(384, (3, 3), padding="same",
+			kernel_regularizer=l2(reg)))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(Conv2D(384, (3, 3), padding="same",
+			kernel_regularizer=l2(reg)))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(Conv2D(256, (3, 3), padding="same",
+			kernel_regularizer=l2(reg)))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+		model.add(Dropout(0.25))
+
+		# Block #4: first set of FC => RELU layers
+		model.add(Flatten())
+		model.add(Dense(4096, kernel_regularizer=l2(reg)))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization())
+		model.add(Dropout(0.5))
+
+		# Block #5: second set of FC => RELU layers
+		model.add(Dense(4096, kernel_regularizer=l2(reg)))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization())
+		model.add(Dropout(0.5))
+
+		# softmax classifier
+		model.add(Dense(classes, kernel_regularizer=l2(reg)))
+		model.add(Activation("softmax"))
+
+		# return the constructed network architecture
+		return model
 
 
 ''' Building GoogleNet '''
