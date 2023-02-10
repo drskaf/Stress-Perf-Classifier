1aad91e243756faf9a53753e5f7373f7def07079
Ebraham Alskaf <73792019+drskaf@users.noreply.github.com>
2023-02-10 15:15:17 +0000

    Update tf_cnn.py

--- tf_cnn.py
+++ tf_cnn.py
@@ -8,6 +8,35 @@ import os
 import time  
 
 
+''' Building Shallow Net '''
+
+class ShallowNet:
+	@staticmethod
+	def build(width, height, depth, classes):
+		# initialize the model along with the input shape to be
+		# "channels last"
+		model = Sequential()
+		inputShape = (height, width, depth)
+
+		# if we are using "channels first", update the input shape
+		if K.image_data_format() == "channels_first":
+			inputShape = (depth, height, width)
+
+		# define the first (and only) CONV => RELU layer
+		model.add(Conv2D(32, (3, 3), padding="same",
+			input_shape=inputShape))
+		model.add(Activation("relu"))
+
+		# softmax classifier
+		model.add(Flatten())
+		model.add(Dense(classes))
+		model.add(Activation("softmax"))
+
+		# return the constructed network architecture
+		return model
+
+	
+
 ''' Building AlexNet '''
 
 class AlexNet:
