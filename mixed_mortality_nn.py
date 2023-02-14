9492cf74fa77735c9d002a61945ad9af281006b9
Ebraham Alskaf <73792019+drskaf@users.noreply.github.com>
2023-02-14 16:15:43 +0000

    Update mixed_mortality_nn.py

--- mixed_mortality_nn.py
+++ mixed_mortality_nn.py
@@ -31,3 +31,22 @@ categorical_col_list = ['Chronic_kidney_disease_(disorder)','Essential_hypertens
 numerical_col_list= ['Age_on_20.08.2021_y', 'LVEF_(%)']
 PREDICTOR_FIELD = args["target"]
 
+# Loading clinical data
+def process_attributes(df, train, valid):
+    continuous = numerical_col_list
+    categorical = categorical_col_list
+    cs = MinMaxScaler()
+    trainContinuous = cs.fit_transform(train[continuous])
+    valContinuous = cs.transform(valid[continuous])
+
+    # One-hot encode categorical data
+    catBinarizer = LabelBinarizer().fit(df[categorical])
+    trainCategorical = catBinarizer.transform(train[categorical])
+    valCategorical = catBinarizer.transform(valid[categorical])
+
+    # Construct our training and testing data points by concatenating
+    # the categorical features with the continous features
+    trainX = np.hstack([trainCategorical, trainContinuous])
+    valX = np.hstack([valCategorical, valContinuous])
+
+    return (trainX, valX)
