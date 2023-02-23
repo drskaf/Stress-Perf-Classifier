5609ff62a2daee433b7e4e7679a1e2fadcf73f07
Ebraham Alskaf <73792019+drskaf@users.noreply.github.com>
2023-02-23 11:42:01 +0000

    Update eda.py

--- eda.py
+++ eda.py
@@ -1,1 +1,20 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
+# Define indices
+dirs_1 = os.listdir('/Users/ebrahamalskaf/Documents/manual_images')
+dirs_2 = os.listdir('/Users/ebrahamalskaf/Documents/test')
+dirs = dirs_2 + dirs_1
+for dir in dirs:
+    if '.DS_Store' in dirs:
+        dirs.remove('.DS_Store')
+if '.\\20220511_indices.csv' in dirs:
+    dirs.remove('.\\20220511_indices.csv')
+
+# Create dataframe
+df = pd.DataFrame(dirs, columns=['index'])
+df['ID'] = df['index'].astype(int)
+patient_df = pd.read_csv('/Users/ebrahamalskaf/Documents/patient_info.csv')
+info_df = pd.merge(df, patient_df, on=['ID'])
