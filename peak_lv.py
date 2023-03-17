2dae1f69224ef9219be3dae4fd243923dba3064a
Ebraham Alskaf <73792019+drskaf@users.noreply.github.com>
2023-03-17 23:08:17 +0000

    Update peak_lv.py

--- peak_lv.py
+++ peak_lv.py
@@ -17,7 +17,14 @@ videos, indices = utils.load_perfusion_data(args["directory"])
 
 # Histogram of pixel intensity
 for video in videos:
-    for i in range(len(video[0])):
-        pix_hist = []
-        pix_sum = np.sum(video[0:i-1,:])
-        pix_hist.append(pix_sum)
+    pix_bins = []
+    indices = []
+    for i in range(len(video[:,])):
+        pix_sum = np.sum(video[i])
+        pix_mean = np.mean(video[i])
+        pix_std = np.std(video[i])
+        pix_norm = pix_sum - pix_mean // pix_std
+        pix_bins.append(pix_norm)
+        indices.append(i)
+    plt.bar(indices, pix_bins, color='blue')
+    plt.show()
