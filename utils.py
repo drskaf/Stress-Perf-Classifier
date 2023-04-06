cb1252830359eff6ba87c9548a7e26b2f61016dc
Ebraham Alskaf <73792019+drskaf@users.noreply.github.com>
2023-04-06 21:17:55 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -153,4 +153,29 @@ def load_perfusion_data(directory):
     return videoSingle_list, indicesSingle, videoStack_list, indicesStack
 
 
+def centre_crop(img, new_width=None, new_height=None):
+
+    width = img.shape[1]
+    height = img.shape[0]
+
+    if new_width is None:
+        new_width = width//2
+
+    if new_height is None:
+        new_height = height//2
+
+    left = int(np.ceil((width - new_width) / 2))
+    right = width - int(np.floor((width - new_width) / 2))
+
+    top = int(np.ceil((height - new_height) / 2))
+    bottom = height - int(np.floor((height - new_height) / 2))
+
+    if len(img.shape) == 2:
+        centre_cropped_img = img[top:bottom, left:right]
+    else:
+        centre_cropped_img = img[top:bottom, left:right, ...]
+
+    return centre_cropped_img
+
+
 
