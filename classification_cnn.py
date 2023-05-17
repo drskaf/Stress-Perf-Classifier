3b07a947c15ac8e4cbd787ada59a91a58e63c6eb
Ebraham Alskaf <73792019+drskaf@users.noreply.github.com>
2023-05-17 08:53:25 +0100

    Update classification_cnn.py

--- classification_cnn.py
+++ classification_cnn.py
@@ -26,14 +26,14 @@ from sklearn.utils import class_weight
 # Command line arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--directory", required=True, help="path to input directory")
-ap.add_argument("-t", "--target", required=True, help="name of the target field")
+ap.add_argument("-t", "--target", required=True, help="target AHA segment")
 args = vars(ap.parse_args())
 
 # Set parameters
 INPUT_DIM = 224
 WIDTH = 224
 HEIGHT = 672
-BATCH_SIZE = 32
+BATCH_SIZE = 64
 NUM_EPOCHS = 500
 N_CLASSES = 1
 
@@ -44,16 +44,26 @@ patient_info['pbasal'] = patient_info[['p_basal anterior','p_basal anteroseptum'
 patient_info['pmid'] = patient_info[['p_mid anterior','p_mid anteroseptum','p_mid inferoseptum','p_mid inferior',
                                        'p_mid inferolateral','p_mid anterolateral']].apply(lambda x: '{}'.format(np.array(x)), axis=1)
 patient_info['papical'] = patient_info[['p_apical anterior', 'p_apical septum','p_apical inferior','p_apical lateral']].apply(lambda x:'{}'.format(np.array(x)), axis=1)
-#.apply(
-    #lambda x: '{}'.format(np.pad(np.array(x), (0,2), 'constant', constant_values=0)), axis=1
-#)
+#format(np.pad(np.array(x), (0,2), 'constant', constant_values=0)), axis=1)
+#print(patient_info['papical'].head())
 
-print(patient_info['papical'].head())
 
           ### Training apical AHA segments classification ###
 
 # Load images and label them
-(info_df) = utils.load_label_png(args["directory"], patient_info, INPUT_DIM)
+(df) = utils.load_multiclass_apical_png(args["directory"], patient_info, INPUT_DIM)
+#print(labels[:5])
+#print(len(labels))
+#print(np.unique(labels, return_counts=True))
+
+#info_df['p_aha'] = patient_info[['p_basal anterior','p_basal anteroseptum','p_basal inferoseptum','p_basal inferior'
+ #                       ,'p_basal inferolateral', 'p_basal anterolateral', 'p_mid anterior','p_mid anteroseptum','p_mid inferoseptum','p_mid inferior',
+  #                                     'p_mid inferolateral','p_mid anterolateral', 'p_apical anterior', 'p_apical septum','p_apical inferior','p_apical lateral']].apply(lambda x:'{}'.format(np.array(x)), axis=1)
+#classes = set(labels)
+#mapper = {k: i + 1 for i, k in enumerate(classes)}
+#patient_info['classes'] = classes.map(mapper)
+#print(patient_info['classes'].head())
+#class_weight = class_weight.compute_class_weight('balanced', classes=np.unique(classes), y=patient_info['papical'].values)
 
 #''' Fine tuning step '''
 
@@ -88,7 +98,7 @@ my_model.add(Dense(1024, activation='relu'))
 my_model.add(Dropout(0.5))
 my_model.add(Dense(512, activation='relu'))
 my_model.add(Dropout(0.5))
-my_model.add(Dense(N_CLASSES, activation='softmax'))
+my_model.add(Dense(N_CLASSES, activation='sigmoid'))
 
 gpus = tf.config.list_physical_devices('GPU')
 if gpus:
@@ -96,35 +106,52 @@ if gpus:
         tf.config.experimental.set_virtual_device_configuration(gpu, [tf.config.experimental.VirtualDeviceConfiguration
                                                                       (memory_limit=4096)])
 # Splitting data
-(df_train, df_valid) = train_test_split(info_df, train_size=0.7, stratify=info_df[args["target"]])
+(df_train, df_valid) = train_test_split(df, train_size=0.7, stratify=df[args['target']])
 X_train = np.array([x / 255.0 for x in df_train['images']])
 X_valid = np.array([z / 255.0 for z in df_valid['images']])
-y_train = np.array(df_train.pop(args["target"]))
+reshaped_X = X_train.reshape(X_train.shape[0],-1)
+oversample = SMOTE(k_neighbors=6, sampling_strategy='minority')
+X_over, y_train = oversample.fit_resample(reshaped_X, df_train[args['target']])
+X_train = X_over.reshape(-1,224,224,1)
+y_train = np.array(y_train)
+print(len(y_train))
 tlist = y_train.tolist()
 print(tlist.count(1))
 print(tlist.count(0))
-y_valid = np.array(df_valid.pop(args["target"]))
+y_valid = np.array(df_valid.pop(args['target']))
 vlist = y_valid.tolist()
 print(vlist.count(1))
+print(vlist.count(0))
 print(y_train[:10])
 print(y_valid[:10])
-# print(class_weight)
-class_weight = {0: 1.0,
-                1: 7.6}
+
+class_weight = {0:0.50,
+                1:3.88}
 
 # Creating class balance on the training dataset
+#X_train = np.array(x / 255.0 for x in df_train['images'])
+#X_valid = np.array(x / 255.0 for x in df_valid['images'])
 #reshaped_X = X_train.reshape(X_train.shape[0],-1)
 #oversample = SMOTE(k_neighbors=6, sampling_strategy='minority')
-#X_over, y_train = oversample.fit_resample(reshaped_X, y_train)
+#classes = set(df_train['labels'])
+#mapper = {k: i + 1 for i, k in enumerate(classes)}
+#df_train['classes'] = classes.map(mapper)
+#print(df_train['classes'].head())
+#X_over, y_train = oversample.fit_resample(reshaped_X, df_train['classes'])
 #X_train = X_over.reshape(-1,224,224,1)
 #print(len(y_train))
-
+#le = LabelEncoder().fit(y_train)
+#y_train = to_categorical(le.transform(y_train), N_CLASSES)
+#print(y_train[:2])
+#le = LabelEncoder().fit(y_valid)
+#y_valid = to_categorical(le.transform(y_valid), N_CLASSES)
+#print(y_valid[:2])
 
 # Data augmentation
-#aug = ImageDataGenerator(samplewise_center=True,samplewise_std_normalization=True,rotation_range=20, width_shift_range=0.1, height_shift_range=0.1, shear_range=0.2, zoom_range
- #                        =0.2, horizontal_flip=True, fill_mode="nearest")
+aug = ImageDataGenerator(samplewise_center=True,samplewise_std_normalization=True,rotation_range=20, width_shift_range=0.1, height_shift_range=0.1, shear_range=0.2, zoom_range
+                         =0.2, horizontal_flip=True, fill_mode="nearest")
 
-#v_aug = ImageDataGenerator(samplewise_center=True,samplewise_std_normalization=True)
+v_aug = ImageDataGenerator(samplewise_center=True,samplewise_std_normalization=True)
 
 # Initialise the optimiser and model
 print("[INFO] compiling model ...")
@@ -143,7 +170,7 @@ Opt = Adam(lr=0.001)
 Loss = BinaryCrossentropy(from_logits=True)
 apical_model = tf_cnns.MiniVGGNet.build(INPUT_DIM, INPUT_DIM, depth=1, classes=N_CLASSES)
 apical_model.compile(loss=Loss, optimizer=Opt, metrics=METRICS)
-weigth_path = "{}_my_model.best.hdf5".format("aha13_MiniVGG")
+weigth_path = "{}_my_model.best.hdf5".format("aha13_MiniVGGNet")
 checkpoint = ModelCheckpoint(weigth_path, monitor='val_prc', save_best_only=True, mode='max', save_weights_only=False)
 early = EarlyStopping(monitor='val_prc', mode='max', patience=20)
 callbacks_list = [checkpoint]
@@ -153,16 +180,15 @@ tensorboard_callback = TensorBoard(log_dir=logdir, histogram_freq=1)
 
 # Training the model
 print("[INFO] Training the model ...")
-history = apical_model.fit(X_train, y_train, batch_size=BATCH_SIZE, validation_data= (X_valid, y_valid), epochs=NUM_EPOCHS,
-                  steps_per_epoch=len(X_train )// 32, callbacks=[early, callbacks_list, tensorboard_callback], verbose=1,
-                           class_weight=class_weight)
+history = apical_model.fit_generator(aug.flow(X_train, y_train, batch_size=BATCH_SIZE), validation_data= v_aug.flow(X_valid, y_valid), epochs=NUM_EPOCHS,
+                  steps_per_epoch=len(X_train )// 64, callbacks=[early, callbacks_list, tensorboard_callback], verbose=1)
 
 # summarize history for loss
 plt.plot(history.history['precision'])
-plt.plot(history.history['val_precision'])
+plt.plot(history.history['val_accuracy'])
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
-plt.title('Mortality CNN training')
+plt.title('AHA13 perfusion classification CNN')
 plt.ylabel('precision')
 plt.xlabel('epoch')
 plt.legend(['train precision', 'validation precision', 'train loss', 'validation loss'], loc='upper left')
@@ -170,8 +196,7 @@ plt.show()
 
 # Saving model data
 model_json = apical_model.to_json()
-with open("aha13_MiniVGG.json", "w") as json_file:
+with open("aha13_MiniVGGNet.json", "w") as json_file:
     json_file.write(model_json)
 
 K.clear_session()
-
