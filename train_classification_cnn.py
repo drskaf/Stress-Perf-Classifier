78649d176d5121469c1dba11ea9d007c59fa4ee0
Ebraham Alskaf <73792019+drskaf@users.noreply.github.com>
2023-06-22 16:58:41 +0100

    Update train_classification_cnn.py

--- train_classification_cnn.py
+++ train_classification_cnn.py
@@ -18,15 +18,12 @@ from keras import backend as K
 from keras.losses import BinaryCrossentropy, SparseCategoricalCrossentropy, CategoricalCrossentropy
 from keras.utils import to_categorical
 import cv2
-from imblearn.over_sampling import RandomOverSampler, SMOTE
-from collections import Counter
 from random import sample
 
 
 # Command line arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--directory", required=True, help="path to input directory")
-#ap.add_argument("-t", "--target", required=True, help="target AHA segment")
 args = vars(ap.parse_args())
 
 # Set parameters
@@ -46,9 +43,6 @@ patient_info = pd.read_csv('/Users/ebrahamalskaf/Documents/patient_info.csv')
 (df) = utils.load_multislice(args["directory"], patient_info, INPUT_DIM)
 print(len(df))
 
-class_weight = {0: 0.574,
-                1: 3.893}
-
 #''' Fine tuning step '''
 
 import ssl
@@ -65,15 +59,10 @@ else:
 from keras.applications.vgg16 import VGG16
 from keras.applications.resnet import ResNet50
 model = VGG16(include_top=False, input_shape=(224, 224, 3), weights='imagenet')
-#model = ResNet50(include_top=False, input_shape=(224, 224, 3), weights='imagenet')
 
 transfer_layer = model.get_layer('block5_pool')
 vgg_model = Model(inputs = model.input, outputs = transfer_layer.output)
-#transfer_layer = model.get_layer('conv5_block3_out')
-#resnet_model = Model(inputs = model.input, outputs = transfer_layer.output)
 
-#for layer in resnet_model.layers:
- #   layer.trainable = False
 for layer in vgg_model.layers[0:17]:
     layer.trainable = False
 my_model = Sequential()
@@ -143,7 +132,7 @@ def my_tf_loss_fn(y_true, y_pred):
 Loss = BinaryCrossentropy()
 model = tf_cnns.LeNet.build(INPUT_DIM, INPUT_DIM, depth=1, classes=N_CLASSES)
 my_model.compile(loss=Loss, optimizer=Opt, metrics=METRICS)
-weigth_path = "{}_my_model.best.hdf5".format("aha4_VGG19")
+weigth_path = "{}_my_model.best.hdf5".format("aha_VGG19")
 checkpoint = ModelCheckpoint(weigth_path, monitor='val_prc', save_best_only=True, mode='max', save_weights_only=False)
 early = EarlyStopping(monitor='val_prc', mode='max', patience=30)
 callbacks_list = [checkpoint]
@@ -169,7 +158,7 @@ plt.show()
 
 # Saving model data
 model_json = my_model.to_json()
-with open("aha4_VGG19.json", "w") as json_file:
+with open("aha_VGG19.json", "w") as json_file:
     json_file.write(model_json)
 
 K.clear_session()
