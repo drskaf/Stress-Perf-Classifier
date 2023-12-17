d308935085709913ad9c957a85bf21f40a9d292c
Ebraham Alskaf <73792019+drskaf@users.noreply.github.com>
2023-12-17 15:24:48 +0000

    Update video_aha_3Dcnn_train.py

--- video_aha_3Dcnn_train.py
+++ video_aha_3Dcnn_train.py
@@ -1,36 +1,37 @@
-import matplotlib
-matplotlib.use("Agg")
-
-from keras.preprocessing.image import ImageDataGenerator
-from keras.layers import AveragePooling2D
-from keras.applications import ResNet50
-from keras.layers import Dropout, Flatten, Dense, Input
-from keras.models import Model
-from keras.optimizers import SGD, Adam
-from sklearn.preprocessing import LabelBinarizer
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-import matplotlib as plt
+import tensorflow as tf
+import keras
+from keras import layers
+import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 import pickle
 import cv2
 import os
-
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--dataset", required=True, help= "path to input dataset")
-ap.add_argument("-m", "--model", required=True, help="path to output serialised model")
-ap.add_argument("-l", "--label-bin", required=True, help="path to output label binariser")
-ap.add_argument("-e", "--epochs", type=int, default=25, help="# of epochs to train our network for")
-ap.add_argument("-p", "--plot", type=str, default="plot.png", help="path to output loss/accuracy plot")
-args = vars(ap.parse_args())
+import pathlib
+import pandas as pd
+import utils
+import tf_cnns
+import tqdm
+import random
+import itertools
+import collections
+import einops
+import remotezip as rz
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+import graphviz
+import pydot
+from tensorflow.keras.optimizers import Adam, SGD, RMSprop
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
+from datetime import datetime
+from keras import backend as K
+from keras.losses import BinaryCrossentropy, SparseCategoricalCrossentropy, CategoricalCrossentropy
 
 # Set parameters
 INPUT_DIM = 224
 WIDTH = 224
 HEIGHT = 224
-DEPTH = 12
+DEPTH = 1
 BATCH_SIZE = 32
 NUM_EPOCHS = 500
 N_CLASSES = 16
@@ -38,6 +39,197 @@ N_CLASSES = 16
 # Load dataframe and creating classes
 patient_info = pd.read_csv('/Users/ebrahamalskaf/Documents/patient_info.csv')
 
-# Load perfusion videos and include them in dataframe
-(df) = utils.load_perf_videos('/Users/ebrahamalskaf/Documents/**PERFUSION_CLASSIFICATION**/STRESS_images', patient_info, INPUT_DIM)
-print(len(df))
+aha_list = ['p_basal anterior','p_basal anteroseptum','p_basal inferoseptum','p_basal inferior'
+                        ,'p_basal inferolateral', 'p_basal anterolateral','p_mid anterior','p_mid anteroseptum','p_mid inferoseptum','p_mid inferior',
+                                       'p_mid inferolateral','p_mid anterolateral','p_apical anterior', 'p_apical septum','p_apical inferior','p_apical lateral']
+
+# Load perfusion videos and their labels
+(videos, labels) = utils.load_perf_videos('/Users/ebrahamalskaf/Documents/**PERFUSION_CLASSIFICATION**/STRESS_images', patient_info, INPUT_DIM, aha_list)
+print(videos.shape)
+print(labels.shape)
+print(anom)
+
+# split data into training and testing
+(trainX, testX, trainY, testY) = train_test_split(videos, labels, test_size=0.25, random_state=42)
+
+# Build 3D CNN
+class Conv2Plus1D(keras.layers.Layer):
+  def __init__(self, filters, kernel_size, padding):
+    """
+      A sequence of convolutional layers that first apply the convolution operation over the
+      spatial dimensions, and then the temporal dimension.
+    """
+    super().__init__()
+    self.seq = keras.Sequential([
+        # Spatial decomposition
+        layers.Conv3D(filters=filters,
+                      kernel_size=(1, kernel_size[1], kernel_size[2]),
+                      padding=padding),
+        # Temporal decomposition
+        layers.Conv3D(filters=filters,
+                      kernel_size=(kernel_size[0], 1, 1),
+                      padding=padding)
+        ])
+
+  def call(self, x):
+    return self.seq(x)
+
+class ResidualMain(keras.layers.Layer):
+  """
+    Residual block of the model with convolution, layer normalization, and the
+    activation function, ReLU.
+  """
+  def __init__(self, filters, kernel_size):
+    super().__init__()
+    self.seq = keras.Sequential([
+        Conv2Plus1D(filters=filters,
+                    kernel_size=kernel_size,
+                    padding='same'),
+        layers.LayerNormalization(),
+        layers.ReLU(),
+        Conv2Plus1D(filters=filters,
+                    kernel_size=kernel_size,
+                    padding='same'),
+        layers.LayerNormalization()
+    ])
+
+  def call(self, x):
+    return self.seq(x)
+
+class Project(keras.layers.Layer):
+  """
+    Project certain dimensions of the tensor as the data is passed through different
+    sized filters and downsampled.
+  """
+  def __init__(self, units):
+    super().__init__()
+    self.seq = keras.Sequential([
+        layers.Dense(units),
+        layers.LayerNormalization()
+    ])
+
+  def call(self, x):
+    return self.seq(x)
+
+def add_residual_block(input, filters, kernel_size):
+  """
+    Add residual blocks to the model. If the last dimensions of the input data
+    and filter size does not match, project it such that last dimension matches.
+  """
+  out = ResidualMain(filters,
+                     kernel_size)(input)
+
+  res = input
+  # Using the Keras functional APIs, project the last dimension of the tensor to
+  # match the new filter size
+  if out.shape[-1] != input.shape[-1]:
+    res = Project(out.shape[-1])(res)
+
+  return layers.add([res, out])
+
+class ResizeVideo(keras.layers.Layer):
+  def __init__(self, height, width):
+    super().__init__()
+    self.height = height
+    self.width = width
+    self.resizing_layer = layers.Resizing(self.height, self.width)
+
+  def call(self, video):
+    """
+      Use the einops library to resize the tensor.
+
+      Args:
+        video: Tensor representation of the video, in the form of a set of frames.
+
+      Return:
+        A downsampled size of the video according to the new height and width it should be resized to.
+    """
+    # b stands for batch size, t stands for time, h stands for height,
+    # w stands for width, and c stands for the number of channels.
+    old_shape = einops.parse_shape(video, 'b t h w c')
+    images = einops.rearrange(video, 'b t h w c -> (b t) h w c')
+    images = self.resizing_layer(images)
+    videos = einops.rearrange(
+        images, '(b t) h w c -> b t h w c',
+        t = old_shape['t'])
+    return videos
+
+input_shape = (None, 120, HEIGHT, WIDTH, DEPTH)
+input = layers.Input(shape=(input_shape[1:]))
+x = input
+
+x = Conv2Plus1D(filters=16, kernel_size=(3, 7, 7), padding='same')(x)
+x = layers.BatchNormalization()(x)
+x = layers.ReLU()(x)
+x = ResizeVideo(HEIGHT // 2, WIDTH // 2)(x)
+
+# Block 1
+x = add_residual_block(x, 16, (3, 3, 3))
+x = ResizeVideo(HEIGHT // 4, WIDTH // 4)(x)
+
+# Block 2
+x = add_residual_block(x, 32, (3, 3, 3))
+x = ResizeVideo(HEIGHT // 8, WIDTH // 8)(x)
+
+# Block 3
+x = add_residual_block(x, 64, (3, 3, 3))
+x = ResizeVideo(HEIGHT // 16, WIDTH // 16)(x)
+
+# Block 4
+x = add_residual_block(x, 128, (3, 3, 3))
+
+x = layers.GlobalAveragePooling3D()(x)
+x = layers.Flatten()(x)
+x = layers.Dense(N_CLASSES)(x)
+
+model = keras.Model(input, x)
+
+model.build(trainX)
+
+# Train the model
+print("[INFO] compiling model ...")
+
+METRICS = [
+    tf.keras.metrics.TruePositives(name='tp'),
+    tf.keras.metrics.FalsePositives(name='fp'),
+    tf.keras.metrics.TrueNegatives(name='tn'),
+    tf.keras.metrics.FalseNegatives(name='fn'),
+    tf.keras.metrics.BinaryAccuracy(name='accuracy'),
+    tf.keras.metrics.Precision(name='precision'),
+    tf.keras.metrics.Recall(name='recall'),
+    tf.keras.metrics.AUC(name='auc'),
+    tf.keras.metrics.AUC(name='prc', curve='PR'),  # precision-recall curve
+]
+Opt = Adam(lr=0.0001)
+Loss = BinaryCrossentropy()
+model.compile(loss=Loss, optimizer=Opt, metrics=METRICS)
+weigth_path = "{}_my_model.best.hdf5".format("models/3Dcnn_aha")
+checkpoint = ModelCheckpoint(weigth_path, monitor='val_prc', save_best_only=True, mode='max', save_weights_only=False)
+early = EarlyStopping(monitor='val_prc', mode='max', patience=20)
+callbacks_list = [checkpoint]
+
+logdir = "logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = TensorBoard(log_dir=logdir, histogram_freq=1)
+
+print("[INFO] Training the model ...")
+history = model.fit(x = trainX, y= trainY, batch_size=BATCH_SIZE, validation_data=(testX, testY),
+                          epochs=NUM_EPOCHS,callbacks=[early, checkpoint, tensorboard_callback],
+                                  verbose=1)
+
+# summarize history for loss
+plt.plot(history.history['prc'])
+plt.plot(history.history['val_prc'])
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('AHA perfusion classification CNN')
+plt.ylabel('prc')
+plt.xlabel('epoch')
+plt.legend(['train prc', 'validation prc', 'train loss', 'validation loss'], loc='upper right')
+plt.show()
+
+# Saving model data
+model_json = model.to_json()
+with open("models/3Dcnn_aha.json", "w") as json_file:
+    json_file.write(model_json)
+
+K.clear_session()
